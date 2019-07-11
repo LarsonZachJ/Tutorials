daeb5ce026c8c192b5c2d7633bf1f7bedd5c9e32
Zach Larson <zlarson@thebancorp.com>
2019-07-11 14:51:31 -0500

    AngularNgRxGettingStarted- Strongly Typed Actions

--- AngularNgRxGettingStarted/src/app/products/product-list/product-list.component.ts
+++ AngularNgRxGettingStarted/src/app/products/product-list/product-list.component.ts
@@ -6,6 +6,7 @@ import { Product } from '@app/products';
 import { ProductService } from '@app/products/service';
 import { Store, select } from '@ngrx/store';
 import * as fromProduct from '@app/products/state';
+import * as productActions from '@app/products/state/actions';
 
 @Component({
   selector: 'app-product-list',
@@ -22,27 +23,23 @@ export class ProductListComponent implements OnInit, OnDestroy {
 
   // Used to highlight the selected product in the list
   selectedProduct: Product | null;
-  sub: Subscription;
 
   checkChanged(value: boolean): void {
-    this.store.dispatch({
-      type: 'TOGGLE_PRODUCT_CODE',
-      payload: value
-    });
+    this.store.dispatch(new productActions.ToggleProductCode(value));
   }
 
   newProduct(): void {
-    this.productService.changeSelectedProduct(this.productService.newProduct());
+    this.store.dispatch(new productActions.InitializeCurrentProduct());
   }
 
   productSelected(product: Product): void {
-    this.productService.changeSelectedProduct(product);
+    this.store.dispatch(new productActions.SetCurrentProduct(product));
   }
 
   ngOnInit(): void {
-    this.sub = this.productService.selectedProductChanges$.subscribe(
-      (selectedProduct) => (this.selectedProduct = selectedProduct)
-    );
+    this.store
+      .pipe(select(fromProduct.getCurrentProduct))
+      .subscribe((currentProduct) => (this.selectedProduct = currentProduct));
 
     this.productService
       .getProducts()
@@ -59,9 +56,7 @@ export class ProductListComponent implements OnInit, OnDestroy {
       });
   }
 
-  ngOnDestroy(): void {
-    this.sub.unsubscribe();
-  }
+  ngOnDestroy(): void {}
 
   constructor(
     private store: Store<fromProduct.State>,
