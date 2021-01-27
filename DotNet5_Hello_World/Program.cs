2402c2ca165accbd38f6d974b26d73fc0bdf1648
Zach Larson <Larson.Zach.J@gmail.com>
2021-01-26 20:44:07 -0600

    Hello world for dotnet 5

---
+++ DotNet5_Hello_World/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DotNet5_Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine($"The current time is {DateTime.Now.TimeOfDay} on {DateTime.Now.Date}");
+        }
+    }
+}
